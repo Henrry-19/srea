5ec0b3bb468b8c27c011b61fe6d78ab6d8934148
Henrry-19 <jimenezhenrry20@gmail.com>
2023-02-19 14:12:13 -0500

    Mejoramiento de presentación de test-v3

--- apps/srea/static/cuestionario/cuestionario.js
+++ apps/srea/static/cuestionario/cuestionario.js
@@ -2,27 +2,29 @@
 const url = window.location.href
 
 const quizBox=document.getElementById('quiz-box')
-
+let cont=1
 $.ajax({
     type:'GET',
     url: `${url}data`,
     success:function(response){
         const data = response.data
-        data.forEach(el => {
+
+        data.forEach((el, id) => {
             for (const [preguntas, respuestas]of Object.entries(el)){
                 quizBox.innerHTML += `
                     <hr>
                     <di class="mb-2">
-                        <b>${preguntas}</b>
+                        <font face="nunito,arial,verdana"><b><b>${id+1 +"."}</b></font>
+                        <font face="nunito,arial,verdana"><b>${preguntas}</b></font>      
                     </di>
                 `
                 respuestas.forEach(respuesta=>{
                     quizBox.innerHTML +=`
-                        <di class="mb-2">
+                        <div class="mb-2">
                         <br>
                             <input type="radio" class="ans" id="${preguntas}-${respuesta}" name="${preguntas}" value="${preguntas}">
                             <label for="${preguntas}">${respuesta}</label>
-                        </di>
+                        </div>
                     `
                 })         
             }
